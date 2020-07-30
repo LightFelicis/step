639b59df663fbc457641fd7af7dd07fc850f7bc5
Alicja Kluczek <alicja2602@gmail.com>
2020-07-30 20:22:58 +0000

    Add user account listeners.

--- portfolio/src/main/webapp/leave-comment/script.js
+++ portfolio/src/main/webapp/leave-comment/script.js
@@ -1,19 +1,44 @@
-function onSignIn(googleUser) {
-  $('#userTokenId').val(googleUser.getAuthResponse().id_token);
-  $('#show-on-sign-in')
-      .css('visibility', 'visible');
-  $('#greeting').text('Welcome, ' + googleUser.getBasicProfile().getName() + '!');
+var auth2; // The Sign-In object.
+var googleUser; // The current user.
+
+function init() {
+  gapi.load('auth2', initSignin);
+}
+
+var initSignin = function() {
+  auth2 = gapi.auth2.getAuthInstance();
+
+  // Setting up the listeners.
+  auth2.isSignedIn.listen(signinChanged);
+  auth2.currentUser.listen(userChanged);
+
+  if (auth2.isSignedIn.get() == true) {
+    auth2.signIn();
+  }
+};
+
+function signinChanged(isSignedIn) {
+  if (isSignedIn) {
+    $('#userTokenId').val(googleUser.getAuthResponse().id_token);
+    $('#show-on-sign-in').css('visibility', 'visible');
+    $('#greeting').text('Welcome, ' + googleUser.getBasicProfile().getName() + '!');
+  } else {
+    $("#userTokenId").val('');
+    $('#show-on-sign-in').css('visibility', 'hidden');
+    $('#greeting').text('Please log in to share your feedback!');
+  }
 }
 
 function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log('User signed out.');
-  });
-  $('#show-on-sign-in')
-      .css('visibility', 'hidden');
-  $("#userTokenId").val('');
+  auth2.signOut();
 }
+
+function userChanged(user) {
+  googleUser = user;
+  if (googleUser.getBasicProfile()) {
+    $('#greeting').text('Welcome, ' + googleUser.getBasicProfile().getName() + '!');
+  }
+};
 
 function getComments() {
   fetch("../comments")
