e33f517491b5d3aedc1768b317949ea7b5385e89
Alicja Kluczek <alicja2602@gmail.com>
2020-08-04 15:00:40 +0200

    Add optional attendee feature.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -18,40 +18,81 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    final List<Event> preparedEvents = prepareEvents(events, request);
+    List<TimeRange> requiredTimeranges = findMeetingTimeranges(events, request.getDuration(),
+        request.getAttendees());
+    List<TimeRange> optionalTimeranges = findMeetingTimeranges(events, request.getDuration(),
+        request.getOptionalAttendees());
+    List<TimeRange> intersection = findTimerangesIntersections(requiredTimeranges,
+        optionalTimeranges, request.getDuration());
+    if (intersection.isEmpty()) {
+      return requiredTimeranges;
+    }
+    return intersection;
+  }
+
+  final List<TimeRange> findTimerangesIntersections(List<TimeRange> required,
+                                                    List<TimeRange> optional,
+                                                    long duration) {
+    List<TimeRange> intersection = new ArrayList<>();
+    for (TimeRange requiredTimeRange : required) {
+      for (TimeRange optionalTimeRange : optional) {
+        Optional<TimeRange> tr = createIntersection(requiredTimeRange, optionalTimeRange);
+        if (tr.isPresent()) {
+          intersection.add(tr.get());
+        }
+      }
+    }
+    return intersection.stream()
+        .filter(timeRange -> timeRange.duration() >= duration)
+        .collect(Collectors.toList());
+  }
+
+  private Optional<TimeRange> createIntersection(TimeRange a, TimeRange b) {
+    Optional<TimeRange> result = Optional.empty();
+    if (a.overlaps(b)) {
+      TimeRange intersection = TimeRange.fromStartEnd(Math.max(a.start(), b.start()),
+          Math.min(a.end(), b.end()), false);
+      result = Optional.of(intersection);
+    }
+    return result;
+  }
+
+  private List<TimeRange> findMeetingTimeranges(Collection<Event> events, long duration,
+                                                Collection<String> attendees) {
+    List<Event> preparedEvents = prepareEvents(events, attendees);
     List<TimeRange> queryResult = new ArrayList<>();
     int currentTime = 0;
-
     for (Event event : preparedEvents) {
-      if (currentTime + request.getDuration() <= event.getWhen().start()) {
-        int end = (int) Math.max(event.getWhen().start(), currentTime + request.getDuration());
+      if (currentTime + duration <= event.getWhen().start()) {
+        int end = (int) Math.max(event.getWhen().start(), currentTime + duration);
         queryResult.add(TimeRange.fromStartEnd(currentTime, end, false));
       }
       currentTime = Math.max(currentTime, event.getWhen().end());
     }
 
-    if (currentTime + request.getDuration() <= TimeRange.END_OF_DAY) {
+    if (currentTime + duration <= TimeRange.END_OF_DAY) {
       queryResult.add(TimeRange.fromStartEnd(currentTime, TimeRange.END_OF_DAY, true));
     }
 
     return queryResult;
   }
 
-  private List<Event> prepareEvents(Collection<Event> events, MeetingRequest request) {
-    List<Event> prepared = filterIrrelevantEvents(events, request);
+  private List<Event> prepareEvents(Collection<Event> events, Collection<String> attendees) {
+    List<Event> prepared = filterIrrelevantEvents(events, attendees);
     sortEvents(prepared);
     return prepared;
   }
 
-  private List<Event> filterIrrelevantEvents(Collection<Event> events, MeetingRequest request) {
+  private List<Event> filterIrrelevantEvents(Collection<Event> events, Collection<String> requestAttendees) {
     return events.stream()
         .filter(event -> {
           Collection<String> eventCopy = new HashSet<>(event.getAttendees());
-          eventCopy.retainAll(request.getAttendees());
+          eventCopy.retainAll(requestAttendees);
           return !eventCopy.isEmpty();
         })
         .collect(Collectors.toList());
