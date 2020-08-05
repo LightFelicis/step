22fa1dbb388e19a370be26c121ea13d6bb5f8e47
Alicja Kluczek <alicja2602@gmail.com>
2020-08-05 14:48:38 +0200

    Replace hard-coded data with fetching static JSON.

--- portfolio/src/main/webapp/my-travels/travels.js
+++ portfolio/src/main/webapp/my-travels/travels.js
@@ -1,54 +1,34 @@
-const placesCoords = {
-    'berlin' : {lat: 52.521999, lng: 13.413016},
-    'default' : {lat: 52.048947, lng: 20.442758},
-    'disneyland' : {lat: 48.869486, lng: 2.780874},
-    'krakow' : {lat: 50.061683, lng: 19.937111},
-    'kolobrzeg' : {lat: 54.186629, lng: 15.570729},
-    'matterhorn' : {lat: 45.938319, lng: 7.731824},
-    'zurich' : {lat: 47.349534, lng: 8.491872}
-    }
-
-const placesDescriptions = {
-    'berlin' : 'My last trip was to Berlin. The photo was taken just after ' +
-        'going shopping, I\'m a huge fan of it, as you can tell from my face.',
-    'default' : 'This is where I live -- ' +
-        'a small town in the centre of Poland.',
-    'disneyland' : 'For my 19th birthday I visited France. ' +
-        'The picture was taken at Disneyland.',
-    'krakow' : 'Kraków is previous capital of Poland and ' +
-        'one of the most beautiful cities I have ever seen.',
-    'kolobrzeg' : 'This is a photo of me and Marek at the beach, ' +
-        'watching sunset.',
-    'zurich' : 'This is a photo from my first trip to Zuerich, ' +
-        'taken at the top of Uetliberg mountain',
-    'matterhorn' : 'This is me in front of Matterhorn mountain, ' +
-        'known from Toblerone.'
-    }
-
 function createDescription(content) {
-  return '<div id="content">' +
-      '<div id="siteNotice">' +
-      '</div>' +
-      '<div id="bodyContent">' +
-      content +
-      '</div>' +
-      '</div>';
+  return `<div id="content">${content}</div>`;
 }
 
 function setLocation(location) {
-  const coords = placesCoords[location];
-  const map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 10, center: coords});
-  const description = createDescription(placesDescriptions[location]);
+  if (typeof setLocation.locationsInfo === 'undefined') {
+    setLocation.locationsInfo = getMap();
+  }
 
-  let infowindow = new google.maps.InfoWindow({
-    content: description
-  });
-  const marker = new google.maps.Marker(
-      {position: coords,
-       map: map,
-       title: location});
-  infowindow.open(map, marker);
+  setLocation.locationsInfo.then(locationsInfo => {
+        const coords = {
+          'lng': locationsInfo['places'][location]['lng'],
+          'lat': locationsInfo['places'][location]['lat']
+        };
+        const map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 10, center: coords});
+        const description =
+            createDescription(locationsInfo['places'][location]['description']);
+
+        const infoWindow = new google.maps.InfoWindow({
+          content: description
+        });
+
+        const marker = new google.maps.Marker({
+          position: coords,
+          map: map,
+          title: location
+        });
+        infoWindow.open(map, marker);
+      }
+  )
 }
 
 // Initialize and add the map to page.
@@ -56,10 +36,22 @@ function initMap() {
   setLocation('default');
 }
 
-Object.keys(placesCoords).forEach((place, _) => {
-  const element = document.getElementById(place);
-  if (element != null) {
-    element.addEventListener('click',
-    () => { setLocation(place); });
-  }
-});
+function getMap() {
+  return fetch("../static/travels-data.json")
+      .then(response => response.text())
+      .then(JSON.parse)
+      .then(addListeners);
+}
+
+function addListeners(placesInfo) {
+  Object.keys(placesInfo['places']).forEach((place, _) => {
+    const element = document.getElementById(place);
+    if (element != null) {
+      element.addEventListener('click',
+          () => {
+            setLocation(place);
+          });
+    }
+  });
+  return placesInfo;
+}
