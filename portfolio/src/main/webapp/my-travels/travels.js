409c56b42a37d78c950284a734110de2b253c962
Alicja Kluczek <alicja2602@gmail.com>
2020-08-06 11:12:36 +0200

    Add pop-ups with descriptions. (#14)
    * Add pop-ups with descriptions.
    
    * Replace hard-coded data with fetching static JSON.

--- portfolio/src/main/webapp/my-travels/travels.js
+++ portfolio/src/main/webapp/my-travels/travels.js
@@ -1,29 +1,57 @@
-const placesCoords = {
-                    "berlin" : {lat: 52.521999, lng: 13.413016},
-                    "default" : {lat: 52.048947, lng: 20.442758},
-                    "disneyland" : {lat: 48.869486, lng: 2.780874},
-                    "krakow" : {lat: 50.061683, lng: 19.937111},
-                    "kolobrzeg" : {lat: 54.186629, lng: 15.570729},
-                    "matterhorn" : {lat: 45.938319, lng: 7.731824},
-                    "zurich" : {lat: 47.349534, lng: 8.491872}
-                   }
+function createDescription(content) {
+  return `<div id="content">${content}</div>`;
+}
 
 function setLocation(location) {
-  const coords = placesCoords[location];
-  const map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 10, center: coords});
-  const marker = new google.maps.Marker({position: coords, map: map});
+  if (typeof setLocation.locationsInfo === 'undefined') {
+    setLocation.locationsInfo = getMap();
+  }
+
+  setLocation.locationsInfo.then(locationsInfo => {
+        const coords = {
+          'lng': locationsInfo['places'][location]['lng'],
+          'lat': locationsInfo['places'][location]['lat']
+        };
+        const map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 10, center: coords});
+        const description =
+            createDescription(locationsInfo['places'][location]['description']);
+
+        const infoWindow = new google.maps.InfoWindow({
+          content: description
+        });
+
+        const marker = new google.maps.Marker({
+          position: coords,
+          map: map,
+          title: location
+        });
+        infoWindow.open(map, marker);
+      }
+  )
 }
 
 // Initialize and add the map to page.
 function initMap() {
-  setLocation("default");
+  setLocation('default');
 }
 
-Object.keys(placesCoords).forEach((place, _) => {
-  const element = document.getElementById(place);
-  if (element != null) {
-    element.addEventListener("click",
-    () => { setLocation(place); });
-  }
-});
+function getMap() {
+  return fetch("../static/travels-data.json")
+      .then(response => response.text())
+      .then(JSON.parse)
+      .then(addListeners);
+}
+
+function addListeners(placesInfo) {
+  Object.keys(placesInfo['places']).forEach((place, _) => {
+    const element = document.getElementById(place);
+    if (element != null) {
+      element.addEventListener('click',
+          () => {
+            setLocation(place);
+          });
+    }
+  });
+  return placesInfo;
+}
